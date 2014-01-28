9b28e7040e73d392b473c41ba891137339ebd745
pyemkey <py.emkey@gmail.com>
2014-01-28 16:22:54 +0100

    Add shouts items

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Shouter::Application.routes.draw do
   root to: "homes#show"
   resource :session, only: [:new, :create, :destroy]
+  resource :dashboard, only: [:show]
   resources :users, only: [:new, :create]
 end
