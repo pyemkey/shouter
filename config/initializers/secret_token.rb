559342c58c78ddd94935dc0c259844c8f8d55256
pyemkey <py.emkey@gmail.com>
2014-01-28 12:52:55 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Shouter::Application.config.secret_key_base = '36d0ab9a39b5ee8a61f24411e6ee1b33b88f56c80eaa1e3c00e727ae60647f0338984d33e2386803ee29659aeb424d4a54aded75bd44cf9cd32bb6d32b846fcf'
