41c66a1863db88454e7730921f1f40dcc96eee71
pyemkey <py.emkey@gmail.com>
2014-01-30 15:20:10 +0100

    Add Following Relationship responsible for followed_by and followers

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Shouter::Application.routes.draw do
   root to: "homes#show"
   resource :session, only: [:new, :create, :destroy]
   resource :dashboard, only: [:show]
-  resources :users, only: [:new, :show, :create]
+  resources :users, only: [:index,:new, :show, :create] do
+  	post 'follow' => 'following_relationships#create'
+  	delete 'follow' => 'following_relationships#destroy'
+  end
   resources :shouts, only: [:show]
   resources :text_shouts, only: [:create]
   resources :photo_shouts, only: [:create]
